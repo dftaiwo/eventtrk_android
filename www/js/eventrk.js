86d74ee58b4fe268ab1a881aaac0fdb81e467eeb
Femi Taiwo <dftaiwo@gmail.com>
2013-08-28 22:19:47 +0100

    Now possible to view a single event and lcik on the registration link if available

--- www/js/eventrk.js
+++ www/js/eventrk.js
@@ -1,4 +1,6 @@
 var serverUrl = 'http://localhost/thinking/eventtrkserver/index.php';
+var eventsData = {}
+
 if (window.location.hostname != 'localhost') {
 
 }
@@ -9,7 +11,11 @@ if (typeof(console) == "undefined") {
         };
 }
 
+var isBlackberry = false;
 
+if (typeof(blackberry) != 'undefined' && typeof(blackberry.invoke) != 'undefined') {
+        isBlackberry = true;
+}
 
 var activeView = '#dashboard';
 $().ready(function() {
@@ -18,14 +24,14 @@ $().ready(function() {
                 handleBrowserStateChange();
         });
 
-        location.href = "#dashboard";
+        goToPage("#dashboard");
 
-        showUpcomingEvents();
+        pullUpcomingEvents();
 });
 
 
 
-function showUpcomingEvents() {
+function pullUpcomingEvents() {
 
         var postData = {
                 action: 'list',
@@ -37,14 +43,35 @@ function showUpcomingEvents() {
                 cache: false,
                 crossDomain: true,
                 dataType: 'json',
-                success: function(data) {
-                        console.log('response', data);
+                success: function(responseData) {
+                        displayUpcomingEvents(responseData);
+                },
+                error: function(xhr, errorType, error) {
+                        alert("Unable to complete request.\n" + error + "\n Please try again");
                 }
         });
-
-
-
 }
+
+function displayUpcomingEvents(responseData){
+        
+        console.log('Displaying Events',responseData);
+        var upcomingDirectives = {
+        Event:{
+                event_teaser:{
+                        html:function(){
+                                
+                                teaser =  '<a href="#singleEvents" onclick="viewEvent('+this.id+');">'+this.description.substr(0,100)+'</a>';
+                                eventsData[this.id] = this;
+                                return teaser;
+                        }
+                }
+        }
+        };
+        
+           $('#eventsList').render(responseData, upcomingDirectives);
+        
+}
+
 
 function handleBrowserStateChange( ) {
 
@@ -64,32 +91,75 @@ function handleBrowserStateChange( ) {
         $(fullPageId).show();
 
 }
-function clearCreateForm(){
+function clearCreateForm() {
         $('#createEventForm').find("input[type=text], textarea").val("");
 }
+
 function postEvent() {
-        
+
         //Assuming validation has been done
         var postData = $('#createEventForm').serialize();
-                   clearCreateForm();
-                   return;
-        console.log(postData);
+        $('#btnCreateEvent').attr('disabled', true);
+        $('#btnCreateEvent').attr('value', 'Please wait...');
         $.ajax({
                 url: serverUrl,
-                data:  postData ,
+                data: postData,
                 type: "POST",
                 cache: false,
                 crossDomain: true,
                 dataType: 'json',
                 success: function(data) {
-                        
+
                         console.log('response', data);
+                        clearCreateForm();
                         alert("Event Saved");
-              
+                        $('#btnCreateEvent').attr('disabled', null);
+                        $('#btnCreateEvent').attr('value', 'Post Event');
+                        goToPage('#upcomingEvents');
                 },
-                        error:function(xhr, errorType, error){
-                                alert("Unable to save.\n"+error + "\n Please try again");
-                        }
+                error: function(xhr, errorType, error) {
+                        $('#btnCreateEvent').attr('disabled', null);
+                        $('#btnCreateEvent').attr('value', 'Post Event');
+                        alert("Unable to save.\n" + error + "\n Please try again");
+                }
         });
+        return false;
+}
+
+function goToPage(pageId) {
+        location.href = pageId;
+}
+
+function viewEvent(eventId){
+        
+        eventInfo = eventsData[eventId];
+        console.log('eventInfo',eventInfo);
+         var upcomingDirectives = {
+        registration:{
+                        html:function(){
+
+                                if(this.registration_link){
+                                        return   '<a onclick="launchExternalPage(\''+this.registration_link+'\');">'+this.registration_link+'</a>';
+                                }
+
+                        }
+                }
+            
+        };
+        
+           $('#singleEvents').render(eventInfo, upcomingDirectives);
+          
+}
+
+function launchExternalPage(pageUrl){
+        if (isBlackberry) {
+                var args = new blackberry.invoke.BrowserArguments(pageUrl);
+                blackberry.invoke.invoke(blackberry.invoke.APP_BROWSER, args);
+        } else {
+                newwindow = window.open(pageUrl, 'name', 'height=400,width=600');
+                if (window.focus) {
+                        newwindow.focus()
+                }
+        }
         return false;
 }

1047c0b9b9c0615dff1a326a4ee56a4bd22f2699
Femi Taiwo <dftaiwo@gmail.com>
2013-08-28 18:54:49 +0100

    WIP - Working on posting new events

--- www/js/eventrk.js
+++ www/js/eventrk.js
@@ -1,9 +1,7 @@
-if (window.location.hostname == 'localhost') {
-
-} else {
+var serverUrl = 'http://localhost/thinking/eventtrkserver/index.php';
+if (window.location.hostname != 'localhost') {
 
 }
-
 if (typeof(console) == "undefined") {
         window.console = {
                 log: function() {
@@ -19,37 +17,79 @@ $().ready(function() {
         $(window).on('hashchange', function() {
                 handleBrowserStateChange();
         });
-        
-        location.href="#dashboard";
-        
+
+        location.href = "#dashboard";
+
         showUpcomingEvents();
 });
 
 
 
-function showUpcomingEvents(){
-        
+function showUpcomingEvents() {
+
+        var postData = {
+                action: 'list',
+        };
+        $.ajax({
+                url: serverUrl,
+                data: JSON.stringify(postData),
+                type: "POST",
+                cache: false,
+                crossDomain: true,
+                dataType: 'json',
+                success: function(data) {
+                        console.log('response', data);
+                }
+        });
+
+
+
 }
 
 function handleBrowserStateChange( ) {
-        
+
         var fullPageId = window.location.hash;
-        
+
         if (!fullPageId || fullPageId == '#')
                 return;
 
         console.log('New Page', fullPageId);
-        
-        if (fullPageId == '#singleEvents' || fullPageId=='#upcomingEvents') { //this is a hack
+
+        if (fullPageId == '#singleEvents' || fullPageId == '#upcomingEvents') { //this is a hack
 //                fullPageId = "#upcomingEvents";
-                
-        } 
+
+        }
         $(activeView).hide();
         activeView = fullPageId;
         $(fullPageId).show();
 
 }
-
-function postEvent(){
+function clearCreateForm(){
+        $('#createEventForm').find("input[type=text], textarea").val("");
+}
+function postEvent() {
+        
+        //Assuming validation has been done
+        var postData = $('#createEventForm').serialize();
+                   clearCreateForm();
+                   return;
+        console.log(postData);
+        $.ajax({
+                url: serverUrl,
+                data:  postData ,
+                type: "POST",
+                cache: false,
+                crossDomain: true,
+                dataType: 'json',
+                success: function(data) {
+                        
+                        console.log('response', data);
+                        alert("Event Saved");
+              
+                },
+                        error:function(xhr, errorType, error){
+                                alert("Unable to save.\n"+error + "\n Please try again");
+                        }
+        });
         return false;
 }
